25dd3b36a58124a4b9d20ba390f146353400c2b4
Vector254 <ochrist7@gmail.com>
2020-07-31 08:40:32 +0300

    add browsing through gallery functionality with javascript

--- js/main.js
+++ js/main.js
@@ -8,3 +8,23 @@
     });
 
 });
+
+  //browse buttons
+  $(document).ready(function(){
+    $("#crust").click(function() {
+      $(".crusts").show();
+      $(".pizzas").hide();
+      $(".toppings").hide();
+    });
+    $("#pizza").click(function() {
+      $(".pizzas").show();
+      $(".crusts").hide();
+      $(".toppings").hide();
+    });
+    $("#topping").click(function() {
+      $(".toppings").show();
+      $(".crusts").hide();
+      $(".pizzas").hide();
+    });
+  });
+  
