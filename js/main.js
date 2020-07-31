0ca6be446239708b17cde01bcca94e8b676ade5e
Vector254 <ochrist7@gmail.com>
2020-07-31 19:57:53 +0300

    add function to clear checkbox after submit

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function resetFields(){
     $("select#pizza-crust").val("");
     $("select#pizza-topping").val("");
     $("input#toppingQty").val("");
-    $("input#delivery").val("");
+    document.getElementById("delivery").checked = false;
     
       }
 function validate(){
